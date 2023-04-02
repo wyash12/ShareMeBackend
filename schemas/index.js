ea3a70bf229f0ab0872487fd56991c1860661c3d
Yash Wankhede <wyash2752@gmail.com>
2023-04-03 02:57:52 +0530

    first commit

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import user from './user'
+import pin from './pin'
+import comments from './comment'
+import postedBy from './postedBy';
+import save from './save'
+export const schemaTypes = [user,pin,comments,postedBy,save]
